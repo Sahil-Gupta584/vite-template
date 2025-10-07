fef5eb9d6dbfc1c78bd849d3c009062fd1e03cb2
Sahil Gupta <guptas3067@gmail.com>
2025-10-07 13:34:34 +0530

    1

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,7 @@ const RootLayout = () => (
       <Link to="/" className="[&.active]:font-bold">
         Home
       </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
+      <Link to="/" className="[&.active]:font-bold">
         About
       </Link>
     </div>
